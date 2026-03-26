99df85b88bae6ce5dc3566f8ac99ffb3b5181109
UNREAL8기\parkh <parkchanheyk@gmail.com>
2026-03-26 11:01:08 +0900

    게임 진행 주기 클래스 모듈화

---
+++ Text_ConsoleRPG/LifeCycle.h
@@ -0,0 +1,31 @@
+// LifeCycle.h
+#pragma once 
+
+class LifeCycle {
+private:
+    // 키 코드 상수
+    static const int KEY_UP = 72;
+    static const int KEY_DOWN = 80;
+    static const int KEY_LEFT = 75;
+    static const int KEY_RIGHT = 77;
+    static const int KEY_ESC = 27;
+
+    bool isRunning;
+    int playerX;
+    int playerY;
+
+    // 내부에서만 사용하는 입력 처리 함수
+    void ProcessInput();
+
+public:
+    // 생성자
+    LifeCycle();
+
+    // 생명주기 함수들
+    void BeginPlay();
+    void Tick();
+    void EndPlay();
+
+    // 상태 확인 함수
+    bool IsRunning() const;
+};
